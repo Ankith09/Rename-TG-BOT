333091d36acf23ba6ce08eededb0c3401bfb0dbd
Ankith09 <62370207+Ankith09@users.noreply.github.com>
2020-08-12 14:38:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,12 +1,12 @@
 class Translation(object):
-    START_TEXT = """ Simple Telegram Renaming BOT Edited By This <a href="https://tx.me/ANKITH_M">MAN</a>
+    START_TEXT = """ Simple Telegram Renaming BOT Edited By @super_botz
 
 <b>Please send me any Telegram file and reply /rename New Name.extn</b>
 SEE /help For My Features"""
 
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @ANKITH_M"
+    UPGRADE_TEXT = "CONTACT @super_botz👦"
     DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
     UPLOAD_START = "Yay, Completed Downloading((个_个)"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @LEECH_GROUP"
@@ -14,17 +14,17 @@ SEE /help For My Features"""
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @PUBLIC_LEECH_2 To Download From Links"
     NOT_AUTH_USER_TEXT = "CONTACT @ANKITH_M"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail (｡◕‿◕｡) "
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = """@LEECH_GROUP"""
+    CUSTOM_CAPTION_UL_FILE = """@super_botz"""
     HELP_USER = """<b>How To Use Me</b>
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
     FEATURES
     👉Rename Telegram File With Custom Thumbnail.
     👉Convert Streamable File(Video) To File.
-    👉FOR Downloading Files At Highspeed Join @LEECH_GROUP"""
+    👉Join our official channel 🗣️ @super_botz """
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
